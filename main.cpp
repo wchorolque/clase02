9e9c9d274830af698c639311d7c1a8fbf7159a82
w. chorolque <wchorolque@gmail.com>
2013-09-04 09:43:29 -0400

    Agregar Metodos para colocar edad y mostrar edad

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+class Gato
+{
+    private:
+        int edad;
+
+    public:
+        void ColocarEdad(int);
+        void MostrarEdad();
+
+};
+
+void Gato::ColocarEdad(int e)
+{
+    edad = e;
+}
+
+void Gato::MostrarEdad()
+{
+    cout << edad << endl;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Gato gato;
+    gato.ColocarEdad(5);
+    cout << "Edad: ";
+    gato.MostrarEdad();
     return 0;
 }
 
